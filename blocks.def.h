21069ed94438d4dbda6b3342f8c0ed03d5c22ed2
Aditya Permana <permanaaditya2606@gmail.com>
2022-11-17 12:41:56 +0700

    back to the old bar without emojis

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,23 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/Scripts/openweather-detailed.sh", 	 600,		 1},
-	{" ", "~/Scripts/kernel", 	 360,		 2},
-	{"  ", "~/Scripts/cpu.sh", 	 15,		 0},
-	{" ", "free -h | awk '/^Mem:/ {print $3\"/\"$2}' | sed s/i//g",	30,		1},
+	{"", "~/Scripts/kernel", 	 360,		 2},
+	{"", "~/Scripts/cpu.sh", 	 15,		 0},
+	{" ", "free -h | awk '/^Mem:/ {print $3\"/\"$2}' | sed s/i//g",	30,		1},
 	{"", "~/Scripts/temp.sh",	60,		0},
+	{"", "~/Scripts/volume",	0,		10},
+	{"", "~/Scripts/micvolume.sh",	0,		10},
+	{"", "~/Scripts/wlan.sh",	1,		1},
 	{"", "~/Scripts/netspeed.sh",	1,		1},
-	{"", "~/Scripts/battery.sh",	15,		0},
-	{"", "~/Scripts/clock",					5,		0},
+	{"", "~/Scripts/battery.sh",	5,		3},
+	{"", "~/Scripts/clock",					60,		1},
+	{"", "~/Scripts/record.sh",					1,		5}
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "   ";
 static unsigned int delimLen = 5;
+
+
+
