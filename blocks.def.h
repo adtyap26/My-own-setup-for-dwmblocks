8a0490c271b277679cf2dfd3aefa8f97ef7397ab
Aditya Permana <permanaaditya2606@gmail.com>
2022-12-25 21:29:36 +0700

    add warp

--- blocks.def.h
+++ blocks.def.h
@@ -4,16 +4,16 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/Scripts/kernel", 	 360,		 2},
 	{"", "~/Scripts/openweather-simple.sh", 	 1800,		 5},
-	{"", "~/Scripts/record.sh",					1,		5},
 	{"", "~/Scripts/cpu.sh", 	 15,		 0},
 	{" ", "free -h | awk '/^Mem:/ {print $3\"/\"$2}' | sed s/i//g",	30,		1},
 	{"", "~/Scripts/temp.sh",	60,		0},
-	{"", "~/Scripts/volume",	0,		10},
-	{"", "~/Scripts/micvolume.sh",	0,		10},
+	{"", "~/Scripts/volumeall.sh",	0,		10},
 	{"", "~/Scripts/wlan.sh",	1,		1},
-	{"", "~/Scripts/netspeed.sh",	1,		1},
+	// {"", "~/Scripts/netspeed.sh",	1,		1},
 	{"", "~/Scripts/battery.sh",	5,		3},
 	{"", "~/Scripts/clock",					60,		1},
+	{"", "~/Scripts/record.sh",					1,		5},
+	{"", "~/Scripts/cloudflarestats.sh",					1,		1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
