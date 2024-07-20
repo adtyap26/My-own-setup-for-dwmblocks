43d2da756e2d17da371fbb41d37f7b00f9620040
Aditya Permana <permanaaditya2606@gmail.com>
2024-07-20 12:57:54 +0700

    update

--- blocks.def.h
+++ blocks.def.h
@@ -1,24 +1,27 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "~/Scripts/kernel", 	 360,		 2},
-	{"", "~/Scripts/openweather-simple.sh", 	 1800,		 5},
-	{"", "~/Scripts/cpu.sh", 	 15,		 0},
-	{" ", "free -h | awk '/^Mem:/ {print $3\"/\"$2}' | sed s/i//g",	30,		1},
-	{"", "~/Scripts/temp.sh",	60,		0},
-	{"", "~/Scripts/volumeall.sh",	0,		10},
-	{"", "~/Scripts/wlan.sh",	1,		1},
-	// {"", "~/Scripts/netspeed.sh",	1,		1},
-	{"", "~/Scripts/battery.sh",	5,		3},
-	{"", "~/Scripts/clock",					60,		1},
-	{"", "~/Scripts/record.sh",					1,		5},
-	{"", "~/Scripts/cloudflarestats.sh",					1,		1},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", "~/Scripts/kernel", 360, 2},
+    {"", "~/Scripts/cpu.sh", 15, 0},
+    {" ", "free -h | awk '/^Mem:/ {print $3\"/\"$2}' | sed s/i//g", 30, 1},
+    {"", "~/Scripts/temp.sh", 60, 0},
+    {"", "~/Scripts/volumeall.sh", 0, 10},
+    {"", "~/Scripts/wlan.sh", 1, 1},
+    // {"", "~/Scripts/netspeed.sh",	1,		1},
+    {"", "~/Scripts/battery.sh", 5, 3},
+    {"", "~/Scripts/clock", 60, 1},
+    // {"",         "~/Scripts/mail_notif.sh", 60, 1},
+    {"", "~/Scripts/record.sh", 1, 5},
+    // {"",         "~/Scripts/cloudflarestats.sh",
+    // 1,		                                 1},
+    {"", "~/Scripts/vpnstats.sh", 1, 1},
+    {"", "~/Scripts/logodwm.sh", 1, 1},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "   ";
-static unsigned int delimLen = 5;
-
-
-
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
+// static char delim[] = "   ";
+static char delim[] = "  ";
+static unsigned int delimLen = 7;
